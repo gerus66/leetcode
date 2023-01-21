12b1146885f4a3a177913cde05d7a6090cc30b01
gerus <gerus66@gmail.com>
2023-01-21 16:45:41 +0100

    biweekly 96

---
+++ easy/2540_minimum_common_value.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/minimum-common-value
+# easy
+# biweekly contest 96
+import List
+
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i, n1, j, n2 = 0, len(nums1), 0, len(nums2)
+        while i < n1 and j < n2:
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            if nums1[i] < nums2[j]:
+                i += 1
+                continue
+            if nums1[i] > nums2[j]:
+                j += 1
+        return -1
+    
